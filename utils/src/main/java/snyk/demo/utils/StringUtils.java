53b2742a96f4b5799235b6d3ee41ccf999005f26
maxjeffos <44034094+maxjeffos@users.noreply.github.com>
2021-03-25 16:07:40 -0400

    fix: fixup to use plugin v2

--- utils/src/main/java/snyk/demo/utils/StringUtils.java
+++ utils/src/main/java/snyk/demo/utils/StringUtils.java
@@ -6,4 +6,12 @@ public class StringUtils {
         System.out.println(s);
     }
 
+    public static String leftPad(String s, char padChar, int numPadChars) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < numPadChars; i++) {
+            sb.append(padChar);
+        }
+        sb.append(s);
+        return sb.toString();
+    }
 }
